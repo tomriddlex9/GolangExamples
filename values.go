3f21d02c245a59c8b7b4a31b6dbc85df9eae59f4
tomriddlex9 <siddhantsiwach1999@gmail.com>
2022-04-21 14:16:21 +0530

    first commit

---
+++ values.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//these are comments
+	// works like in c and cpp
+	/* A multi line comment
+	just like C++
+	*/
+	fmt.Println("go" + "lang") // string concatenation
+	fmt.Println("1+1= ", 1+1)
+	fmt.Println(true && false)
+	fmt.Println("This gets printed in a newline because of Println")
+	fmt.Print("this & ")
+	fmt.Print("this gets printed in same line, because of Print")
+	fmt.Println()
+	fmt.Println("Now a new line")
+	fmt.Println(!true)
+
+}
